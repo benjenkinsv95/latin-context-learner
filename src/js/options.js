5ac818a6b94e34d1d8f45de7a01bc09c26103ffe
benjenkinsv95 <benjenkinsv95@gmail.com>
2020-12-27 23:33:58 -0500

    Remove source and target options since they dont work

--- src/js/options.js
+++ src/js/options.js
@@ -1,35 +1,32 @@
-import "../css/options.css";
+import '../css/options.css'
 // docs: https://developer.chrome.com/docs/extensions/mv2/options/#:~:text=A%20user%20can%20view%20an,then%20selection%20the%20options%20link.
 // Saves options to chrome.storage
-function save_options() {
-  const username = document.getElementById('username').value;
-  const sourceLanguage = document.getElementById('source-language').value;
-  const targetLanguage = document.getElementById('target-language').value;
+function saveOptions () {
+  const username = document.getElementById('username').value
 
-  chrome.storage.sync.set({ username, sourceLanguage, targetLanguage }, function() {
+  // eslint-disable-next-line no-undef
+  chrome.storage.sync.set({ username }, function () {
     // Update status to let user know options were saved.
-    const status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    const status = document.getElementById('status')
+    status.textContent = 'Options saved.'
+    setTimeout(function () {
+      status.textContent = ''
+    }, 750)
+  })
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
+function restoreOptions () {
   // Use default value color = 'red' and likesColor = true.
+  // eslint-disable-next-line no-undef
   chrome.storage.sync.get({
-    username: '',
-    sourceLanguage: '',
-    targetLanguage: ''
-  }, function({ username, sourceLanguage, targetLanguage }) {
-    document.getElementById('username').value = username;
-    document.getElementById('source-language').value = sourceLanguage;
-    document.getElementById('target-language').value = targetLanguage;
-  });
+    username: ''
+  }, function ({ username }) {
+    document.getElementById('username').value = username
+  })
 }
-document.addEventListener('DOMContentLoaded', restore_options);
+
+document.addEventListener('DOMContentLoaded', restoreOptions)
 document.getElementById('save').addEventListener('click',
-    save_options);
+  saveOptions)
